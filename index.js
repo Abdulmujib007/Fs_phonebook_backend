070c14277b4b52ee374b32d2f8aba9be52d5be45
Abdulmujib <asiyanbiakorede@gmail.com>
2021-07-31 19:50:34 +0100

    phonebook_backend step4

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require('express')
 
 const server = express()
+server.use(express.json())
 
-const persons = [
+let persons = [
     { 
-      "id": 1,
+      "id":1,
       "name": "Arto Hellas", 
       "number": "040-123456"
     },
     { 
-      "id": 2,
+      "id":2,
       "name": "Ada Lovelace", 
       "number": "39-44-5323523"
     },
     { 
-      "id": 3,
+      "id":3,
       "name": "Dan Abramov", 
       "number": "12-43-234345"
     },
     { 
-      "id": 4,
+      "id":4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
@@ -32,6 +33,43 @@ const persons = [
 server.get('/api/persons', (req,res) => {
     res.send(persons)
 })
+ 
+server.get(`/api/persons/:id`, (req,res) => {
+     const id = Number(req.params.id.replace(':',''))
+     const person =persons.find(person => person.id == id)
+     if(person){
+      res.send(person)
+     } else {
+       res.status(404).end()
+     }
+})
+
+server.get('/info', (req,res) => {
+  res.send(`<div>
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>    
+  </div>`
+  )
+})
+server.delete('/api/persons/:id', (req,res) => {
+  const id  = Number(req.params.id)
+   persons =  persons.filter(person => person.id !== id)
+   res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = Math.max(...persons.map(person => person.id)) + 1
+  return maxId
+
+}
+
+ server.post('/api/person',(req,res) => {
+   const person = req.body
+   person.id = generateId()
+   persons = persons.concat(person)
+   res.send(person)
+ })
+
 
 server.listen('3001', () => {
     console.log(`server is running on http/localhost:3001`)
